2d7454d1070f2a2a0adbebd80e3700f7cd1be9c8
yinwei <980165003@qq.com>
2019-03-05 14:07:26 +0800

    登陆页面优化

--- src/components/page/Login.vue
+++ src/components/page/Login.vue
@@ -3,19 +3,24 @@
         
         <div class="ms-login">
             <div class="ms-title">
-                <p class="ms-login-title">偷偷美管理平台登陆</p>
+                <p class="ms-login-title">偷偷美&nbsp;管理平台登陆</p>
+                <img src="/static/img/ms-title-icon.png" width="10" class="ms-title-icon">
             </div>
             <el-form :model="ruleForm" size="medium" :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm">
                 <el-form-item prop="user_name">
-                    <el-input v-model="ruleForm.user_name" placeholder="username"></el-input>
+                    <el-input v-model="ruleForm.user_name" placeholder="username">
+                        <i slot="prefix" class="el-input__icon el-icon-user"></i>
+                    </el-input>
                 </el-form-item>
                 <el-form-item prop="user_pwd">
-                    <el-input type="password" placeholder="password" v-model="ruleForm.user_pwd" @keyup.enter.native="submitForm('ruleForm')"></el-input>
+                    <el-input type="password" placeholder="请输入密码" v-model="ruleForm.user_pwd" @keyup.enter.native="submitForm('ruleForm')">
+                        <i slot="prefix" class="el-input__icon el-icon-pass"></i>
+                    </el-input>
                 </el-form-item>
                 <div class="login-btn">
-                    <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+                    <el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
                 </div>
-                <p style="font-size:12px;line-height:30px;color:#999;">Tips : 用户名和密码随便填。</p>
+                <!-- <p style="font-size:12px;line-height:30px;color:#999;">Tips : 用户名和密码随便填。</p> -->
             </el-form>
         </div>
     </div>
@@ -78,35 +83,89 @@
     .ms-title{
         width:100%;
         height: 30px;
-        
         text-align: center;
         font-size:20px;
         color: #fff;
         margin-bottom: 25px;
+        position: relative;
+        left: 0px;
+        top: 0px;
     }
     .ms-login-title{
         line-height: 30px;
+    }
+    .ms-title-icon{
+        position: absolute;
+        left: 106px;
+        top: -1px;
+        z-index: 9;
     }
     .ms-login{
         position: absolute;
         right:11%;
         top:37%;
         width:280px;
-        height:250px;
-        padding:20px 60px;
-        border-radius: 5px;
-        background:rgba(34,26,61,0.5);
+        height:240px;
+        padding:25px 60px;
+        border-radius: 8px;
+        background:rgba(35,27,62,0.6);
     }
-   /* .ms-input .el-input__inner{
-        background:rgba(61,53,86,0.5);
-        width: 100%;
-        height: 20px;
-    } */
-    .login-btn{
-        text-align: center;
+    
+</style>
+<style>
+    .el-icon-user{
+        background: url('/static/img/login-user-icon.png') center no-repeat; 
+        background-size: cover;
+        height:20px;
+        width:16px;
+        margin-left:12px;
+        margin-top:8px;
+
+    }
+    .el-icon-user:before{
+        content: "";
+        font-size: 16px;
+        visibility: hidden;
+    }
+    .el-icon-pass{
+        background: url('/static/img/login-pass-icon.png') center no-repeat; 
+        background-size: cover;
+        height:20px;
+        width:17px;
+        margin-left:12px;
+        margin-top:8px;
+
+    }
+    .el-icon-pass:before{
+        content: "";
+        font-size: 16px;
+        visibility: hidden;
+    }
+    .el-input--prefix .el-input__inner{
+        padding-left:40px;
+    }
+    .el-input__inner{
+        background-color:rgba(61,54,86,0.4);
+        border-radius:20px;
+        color:#fff;
     }
     .login-btn button{
-        width:100%;
-        height:36px;
+        width:166px;
+        height: 40px;
+        padding:0px;
+        text-align:center;
+        display:block;
+        margin:0 auto;
+        border-radius:20px;
+    }
+    .el-button--primary{
+        width:166px;
+        height: 40px;
+        line-height:40px;
+        font-size: 16px;
+        color:#fff;
+    }
+    .el-form-item{
+        margin-bottom:30px;
     }
 </style>
