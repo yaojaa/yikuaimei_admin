57b743c4931afbf5874e270c36cdaae5b1f9f353
yinwei <980165003@qq.com>
2019-05-27 17:36:52 +0800

    map

--- static/js/TMap.js
+++ static/js/TMap.js
@@ -1,13 +1,17 @@
 export function TMap() {
     return new Promise(function(resolve, reject) {
-      window.init = function() {
-        resolve(qq)
-      }
+      // window.init = function() {
+      //   resolve(qq)
+      // }
       var script = document.createElement('script')
       script.type = 'text/javascript'
       script.src = 'http://map.qq.com/api/js?v=2.exp&callback=init&key=SYABZ-FPFRG-JC4QQ-IURT5-FOQHH-3HFCI&libraries=place'
       script.onerror = reject
       document.head.appendChild(script)
+      script.onload = function(){
+        console.log(11111)
+        resolve(qq)
+      }
     })
   }
   
